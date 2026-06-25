7cbaa69e86c9e489856658efb0e0c914c110321e
bryanybenjivillacis-netizen <bryanybenjivillacis@gmail.com>
2026-06-25 17:11:33 -0600

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 TOKEN = os.getenv("DISCORD_TOKEN")
-OWNER_ID = int(os.getenv("OWNER_ID", "0"))
+OWNER_ID = int(os.getenv("893295635837644810", "0"))
 DATABASE_URL = os.getenv("DATABASE_URL")
 PREFIX = os.getenv("PREFIX", ".")
