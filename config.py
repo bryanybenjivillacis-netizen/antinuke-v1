46ed771f823af89ec3e5e57ba1e11a2a4a0dd39c
bryanybenjivillacis-netizen <bryanybenjivillacis@gmail.com>
2026-06-25 21:36:05 -0600

    Update DATABASE_URL to use environment variable

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ load_dotenv()
 
 TOKEN = os.getenv("DISCORD_TOKEN")
 OWNER_ID = int(os.getenv("893295635837644810", "1"))
-DATABASE_URL = os.getenv("postgresql://postgres:NgqYVwVzSTQeWudBmPoWaCKGFhEfLbBn@postgres.railway.internal:5432/railway")
+DATABASE_URL = os.getenv("DATABASE_URL")
 PREFIX = os.getenv("PREFIX", ".")
