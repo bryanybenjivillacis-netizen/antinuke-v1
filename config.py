84d3b152428bc58cb81d2a934acdfbbc8d77070b
bryanybenjivillacis-netizen <bryanybenjivillacis@gmail.com>
2026-06-25 21:34:33 -0600

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 TOKEN = os.getenv("DISCORD_TOKEN")
-OWNER_ID = int(os.getenv("893295635837644810", "0"))
-DATABASE_URL = os.getenv("DATABASE_URL")
+OWNER_ID = int(os.getenv("893295635837644810", "1"))
+DATABASE_URL = os.getenv("postgresql://postgres:NgqYVwVzSTQeWudBmPoWaCKGFhEfLbBn@postgres.railway.internal:5432/railway")
 PREFIX = os.getenv("PREFIX", ".")
